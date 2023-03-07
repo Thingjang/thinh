8305e0d5a090c3a05834d1f18fa6c6071b320ab4
Thingjang <118869957+Thingjang@users.noreply.github.com>
2023-03-07 22:52:37 +0700

    Add files via upload
    huhu

---
+++ config.js
@@ -0,0 +1,10 @@
+const nameGirl = '>..<';
+const giftUrl = 'https://www.youtube.com/watch?v=b3GCfyNKeLs';
+const eventName = '8/3 nè';
+const titleCard = 'tặng Hoa nè';
+const contentCard = 'yeahhh 8/3 vui vẻ có nc rùi đi tắm hui há há há:>>>>>>> cíu con cíu con';
+
+
+const giftImage = 'tubi.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
